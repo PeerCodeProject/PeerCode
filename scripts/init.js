55fef6ae995fc0657b677a6b1dadc1f9362dd930
Liquidibrium <vanoganjelashvili@gmail.com>
2022-05-29 22:47:25 +0400

    Update init script

--- scripts/init.js
+++ scripts/init.js
@@ -2,40 +2,38 @@
 
 const fs = require("fs");
 const path = require("path");
-// npm install --build-from-source
-// path join
+
 
 const DEBUG_WRTC_FOLDER = path.join(
-  __dirname,
-  "..",
-  "node_modules",
-  "wrtc",
-  "build",
-  "Debug"
+    __dirname,
+    "..",
+    "node_modules",
+    "wrtc",
+    "build",
+    "Debug"
 );
 const RELEASE_WRTC_FOLDER = path.join(
-  __dirname,
-  "..",
-  "node_modules",
-  "wrtc",
-  "build",
-  "Release"
+    __dirname,
+    "..",
+    "node_modules",
+    "wrtc",
+    "build",
+    "Release"
 );
 
 // if debug folder not exists then create it and copy content from release folder
 if (!fs.existsSync(DEBUG_WRTC_FOLDER)) {
-  fs.mkdirSync(DEBUG_WRTC_FOLDER);
-  fs.copyFileSync(
-    path.join(RELEASE_WRTC_FOLDER, "wrtc.node"),
-    path.join(DEBUG_WRTC_FOLDER, "wrtc.node")
-  );
-
-  fs.copyFileSync(
-    path.join(RELEASE_WRTC_FOLDER, "wrtc.lib"),
-    path.join(DEBUG_WRTC_FOLDER, "wrtc.lib")
-  );
-  fs.copyFileSync(
-    path.join(RELEASE_WRTC_FOLDER, "wrtc.exp"),
-    path.join(DEBUG_WRTC_FOLDER, "wrtc.exp")
-  );
+    fs.mkdirSync(DEBUG_WRTC_FOLDER);
+    fs.copyFileSync(
+        path.join(RELEASE_WRTC_FOLDER, "wrtc.node"),
+        path.join(DEBUG_WRTC_FOLDER, "wrtc.node")
+    );
+    const wrtcLib = path.join(RELEASE_WRTC_FOLDER, "wrtc.lib");
+    if (!fs.existsSync(wrtcLib)) {
+        fs.copyFileSync(wrtcLib, path.join(DEBUG_WRTC_FOLDER, "wrtc.lib"));
+    }
+    const wrtcExp = path.join(RELEASE_WRTC_FOLDER, "wrtc.exp");
+    if (!fs.existsSync(wrtcExp)) {
+        fs.copyFileSync(wrtcExp, path.join(DEBUG_WRTC_FOLDER, "wrtc.exp"));
+    }
 }
