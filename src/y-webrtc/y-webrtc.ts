37306750f88abbdb279fdc5b5aec888a05cf2f71
Liquidibrium <vanoganjelashvili@gmail.com>
2022-05-30 09:11:19 +0400

    add ICE servers

--- src/y-webrtc/y-webrtc.ts
+++ src/y-webrtc/y-webrtc.ts
@@ -649,7 +649,12 @@ export class WebrtcProvider extends Observable<string> {
     this.shouldConnect = false;
     this.signalingConns = [];
     this.maxConns = 20 + math.floor(random.rand() * 15);
-    this.peerOpts = { wrtc: require("wrtc") };
+    this.peerOpts = {
+      wrtc: require("wrtc"),
+      config : {
+        iceServers: ICE_SERVERS
+      }
+    };
 
     this.room = openRoom(doc, this, roomName, null);
     this.connect();
@@ -707,3 +712,44 @@ export class WebrtcProvider extends Observable<string> {
     super.destroy();
   }
 }
+
+
+const ICE_SERVERS: RTCIceServer[] =
+  [
+    {
+      urls: [
+        'stun:stun.l.google.com:19302',
+        'stun:stun1.l.google.com:19302',
+        'stun:stun2.l.google.com:19302',
+        'stun:stun3.l.google.com:19302',
+        'stun:stun4.l.google.com:19302',
+        'stun:global.stun.twilio.com:3478'
+      ],
+
+    },
+    {
+      urls: 'turn:numb.viagenie.ca',
+      credential: 'muazkh',
+      username: 'webrtc@live.com'
+    },
+    {
+      urls: 'turn:192.158.29.39:3478?transport=udp',
+      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      username: '28224511:1379330808'
+    },
+    {
+      urls: 'turn:192.158.29.39:3478?transport=tcp',
+      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      username: '28224511:1379330808'
+    },
+    {
+      urls: 'turn:turn.bistri.com:80',
+      credential: 'homeo',
+      username: 'homeo'
+    },
+    {
+      urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
+      credential: 'webrtc',
+      username: 'webrtc'
+    }
+  ];
