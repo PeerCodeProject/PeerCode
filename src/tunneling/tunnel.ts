b5e452c4ecb7aea8555db393957f8d64f04ffc29
Liquidibrium <vanoganjelashvili@gmail.com>
2022-06-23 13:46:43 +0400

    Update tunneling to share images as Base64 string

--- src/tunneling/tunnel.ts
+++ src/tunneling/tunnel.ts
@@ -1,8 +1,8 @@
 import * as http from 'http';
 import stream = require('stream');
 
-import fetch, { RequestInit } from "node-fetch";
-import { Observable } from 'lib0/observable';
+import fetch, {RequestInit, Response} from "node-fetch";
+import {Observable} from 'lib0/observable';
 import * as vscode from 'vscode';
 
 export function tunnelClient(provider: Observable<string>) {
@@ -15,11 +15,27 @@ export function tunnelClient(provider: Observable<string>) {
         provider.emit("clientRequest", [request]);
 
         const responseHandler = (data1: string) => {
-            console.log("responseHandler:" + data1);
-            provider.off("tunneledServerResponse", responseHandler);
-            const resp = JSON.parse(data1);
-            res.writeHead(resp.status, resp.statusText, resp.headers);
-            res.write(resp.data);
+            try {
+                console.log("responseHandler:" + data1);
+                provider.off("tunneledServerResponse", responseHandler);
+                // let dataVal = null;
+                const resp = JSON.parse(data1);
+
+                resp.headers = resp.headers || {};
+                const data = getData(resp);
+
+                res.writeHead(resp.status, resp.statusText, resp.headers);
+                res.write(data);
+            } catch (error) {
+                if (error instanceof Error) {
+                    res.writeHead(500, "Internal Server Error", {
+                        // eslint-disable-next-line @typescript-eslint/naming-convention
+                        "Content-Type": "text/plain"
+                    });
+                    res.write("PEERCODE Error: " + error.message);
+                    console.error(error.message);
+                }
+            }
             res.end();
         };
 
@@ -52,32 +68,67 @@ export function tunnelServer(provider: Observable<string>, port: number) {
     provider.emit("startSharingPort", [port]);
 
     const handleTunneledRequestData = async (data1: string) => {
-        const data = JSON.parse(data1);
-        console.log(`Tunneled Request ${JSON.stringify(data, null, 2)}`);
-        const url = `http://localhost:${port}${data.url}`;
+        try {
 
-        const reqInit: RequestInit = {
-            headers: data.headers,
-            method: data.method,
-        };
-        if (data.data !== undefined && data.data !== "") {
-            reqInit.body = data.data;
+            const data = JSON.parse(data1);
+            console.log(`Tunneled Request ${JSON.stringify(data, null, 2)}`);
+            const url = `http://localhost:${port}${data.url}`;
+
+            const reqInit: RequestInit = {
+                headers: data.headers,
+                method: data.method,
+            };
+            if (data.data !== undefined && data.data !== "") {
+                reqInit.body = data.data;
+            }
+            const res = await fetch(url, reqInit);
+            console.log(`Local response ${JSON.stringify(res, null, 2)}`);
+
+            const contentData = await getContentData(res);
+            console.log("contentData: " + contentData);
+
+            provider.emit("serverResponse", [JSON.stringify({
+                ok: res.ok,
+                status: res.status,
+                statusText: res.statusText,
+                headers: res.headers.raw(),
+                data: contentData
+                // data: new Uint8Array( await res.arrayBuffer())
+            })]);
+
+
+        } catch (error) {
+            if (error instanceof Error) {
+                provider.emit("serverResponse", [JSON.stringify({
+                    ok: false,
+                    status: 500,
+                    statusText: "Internal Server Error",
+                    headers: {
+                        // eslint-disable-next-line @typescript-eslint/naming-convention
+                        "Content-Type": "text/plain"
+                    },
+                    data: "PEERCODE Error: " + error.message
+                })]);
+                console.error(error.message);
+            }
         }
-        const res = await fetch(url, reqInit);
-        console.log(`Local response ${JSON.stringify(res, null, 2)}`);
-
-        provider.emit("serverResponse", [JSON.stringify({
-            ok: res.ok,
-            status: res.status,
-            statusText: res.statusText,
-            headers: res.headers.raw(),
-            data: await res.text()
-        })]);
-
-
         console.log('sent response');
     };
     provider.on("tunneledClientRequest", handleTunneledRequestData);
+}
+
+async function getContentData(res: Response) {
+    return shouldGetBytes(res.headers) ?
+        Buffer.from(await res.arrayBuffer()).toString("base64") :
+        res.text();
+}
+
+function shouldGetBytes(headers: Headers) {
+    const contentType = headers.get("Content-Type");
+    if (contentType === undefined || contentType === null) {
+        return false;
+    }
+    return contentType.includes("image");
 }
 
 function getRequestObject(content: unknown, req: http.IncomingMessage) {
@@ -121,3 +172,19 @@ function getContent(readable: stream.Readable) {
     });
 
 }
+
+function getData(resp: any): string | Buffer {
+    if (resp.data === undefined || resp.data === null) {
+        return "";
+    }
+    const contentType = resp.headers["content-type"] || resp.headers["Content-Type"];
+    if (contentType === undefined || contentType === null) {
+        return resp.data;
+    }
+
+    if (contentType[0].includes("image")) {
+        return Buffer.from(resp.data, "base64");
+    }
+    return resp.data;
+}
+
